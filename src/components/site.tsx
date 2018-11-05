b8e7a5441c371c6ed1f3e26c1f4fc030381da1e4
Mike Chen <bobby@Mikes-MacBook-Pro.local>
2018-11-04 23:00:38 -0800

    add some charts/visualizations

--- src/components/site.tsx
+++ src/components/site.tsx
@@ -1,8 +1,36 @@
 import * as React from "react";
 import { allStates } from "../models/economy-model";
+import ReactDOM from 'react-dom';
+import { VictoryBar, VictoryStack, VictoryAxis, VictoryChart } from 'victory';
+
+// const data = [
+//     {quarter: 1, earnings: 10},
+//     {quarter: 2, earnings: 30},
+//     {quarter: 3, earnings: 60},
+//     {quarter: 4, earnings: 70}
+//   ];
 
 export const Site = () => <div>
     <h1>heartpoints.org Model</h1>
-    <p>Model Run Resulttt</p>
+    <p>Model Run Result</p>
     <pre>{JSON.stringify(allStates, null, 3)}</pre>
+    <VictoryChart>
+        <VictoryStack>
+            <VictoryBar
+                data={allStates}
+                x="timestep"
+                y="people[0].balance"
+            />
+            <VictoryBar
+                data={allStates}
+                x="timestep"
+                y="people[1].balance"
+            />
+            <VictoryBar
+                data={allStates}
+                x="timestep"
+                y="people[2].balance"
+            />
+        </VictoryStack>
+      </VictoryChart>
 </div>
