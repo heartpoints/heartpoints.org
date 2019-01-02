75f05806e57dacb6df210ad6691941580327e025
Mike Chen <bobby@Mikes-MacBook-Pro.local>
2019-01-01 16:27:54 -0800

    more refactoring

--- heartpoints.sh
+++ heartpoints.sh
@@ -9,19 +9,6 @@ heartpoints() { local command=$1; local remainingArgs=${@:2}
     fi
 }
 
-heroku_login() {
-    heroku_cli login --interactive
-}
-
-heroku_applicationName() {
-    echo "heartpoints-org"
-}
-
-heartpoints_tailProductionLogs() {
-    heroku_login
-    heroku_cli logs --app $(heroku_applicationName) --tail
-}
-
 heartpoints_help() {
     echo ""
     echo "Usage: heartpoints.sh [command]"
@@ -29,24 +16,19 @@ heartpoints_help() {
     echo "Commands:"
     echo ""
     echo "createGKECluster            - creates a GKE cluster. See README for prerequisites"
-    echo "dev                         - run dev web server locally"
-    echo "manualDeploy <gitSha>       - interactive interview to deploy to production (will prompt for heroku credentials)"
-    echo "prePushVerification         - validates that local code is ready for pull request"
-    echo "tailProductionLogs          - tail the logs from production to see how server is performing"
+    echo "localDev                    - run dev web server locally"
+    echo "manualDeploy <gitSha>       - interactive interview to deploy to production"
+    echo "minikubeBuildDeployTest     - using minikube's docker daemon, build image, then deploy and test"
+    echo "minikubeDestroyEnvironment  - if minikube dev environment is running, destroys it"
+    echo "minikubeOpenWebsite         - assuming site is running in minikube locally, open web browser to home page"
+    echo "minikubeRunTests            - run tests against an existing minikube-hosted website"
     echo "model                       - outputs a sequence of states describing the evolution of the heartpoints ecosystem"
+    echo "prePushVerification         - validates that local code is ready for pull request"
     echo "yarn                        - call the heartpoints-specific version of yarn to add / remove dependencies, etc"
-    echo "buildAndDeployToMinikube    - using minikube's docker daemon, build image, then deploy and test"
-    echo "openMinikubeWebsite         - assuming site is running in minikube locally, open web browser to home page"
-    echo "destroyMinikubeEnvironment  - if minikube dev environment is running, destroys it"
-    echo "testAgainstMinikube         - run tests against an existing minikube-hosted website"
     echo ""
 }
 
-string_is_empty() { local possiblyEmptyString=$1
-    [[ -z "${possiblyEmptyString}" ]]
-}
-
-heartpoints_dev() {
+heartpoints_localDev() {
     heartpoints_prepareForRun
     heartpoints_runServer
 }
@@ -66,39 +48,13 @@ heartpoints_yarn() { local args=$@
     fi
     yarn ${args}
 }
-heartpoints_nodejs_ensureCorrectVersion() {
-    nodejs_ensureCorrectVersion
-}
-
-nodejs_ensureCorrectVersion() {
-    if command_does_not_exist "node" || ! strings_are_equal "$(node -v)" "$(cat .nvmrc)"; then
-        nvm_installAndUseVersionInNvmRC
-    fi
-}
-
-nvm_installAndUseVersionInNvmRC() {
-    nvm_load
-    set +e
-    nvm install 
-    set -e
-    nvm use
-}
-
-npm_cli() { local args=$@
-    nodejs_ensureCorrectVersion   
-    npm ${args}
-}
-
-heartpoints_dev_url() {
-    echo "http://localhost:5001"
-}
 
 heartpoints_buildAndTagImage() { local imageURI=$1
-    echo "Please ensure Docker daemon is running"
-    docker build --build-arg commitSha="$(git_commitSha)" -t ${imageURI} .
+    echo "Please ensure Docker daemon is running if it is not"
+    docker build --build-arg commitSha="$(git_currentSha)" -t ${imageURI} .
 }
 
-heartpoints_testImage() { local imageURI=$1
+heartpoints_dockerTestImage() { local imageURI=$1
     local testName="heartpointsTest"
     trap "docker stop ${testName} > /dev/null" EXIT
     docker run --detach --name "${testName}" --rm "${imageURI}"
@@ -107,78 +63,69 @@ heartpoints_testImage() { local imageURI=$1
 }
 
 heartpoints_prePushVerification() {
-    heartpoints_buildAndDeployToMinikube
+    heartpoints_minikubeBuildDeployTest
 }
 
 heartpoints_onPullRequest() {
-    heartpoints_buildTagAndTest
+    heartpoints_dockerBuildTagAndTest
 }
 
-heartpoints_buildTagAndTest() {
-    local imageURI="heartpoints.org:$(git_commitSha)"
+heartpoints_dockerBuildTagAndTest() {
+    local imageURI="heartpoints.org:$(git_currentSha)"
     heartpoints_buildAndTagImage "${imageURI}"
-    heartpoints_testImage "${imageURI}"
+    heartpoints_dockerTestImage "${imageURI}"
 }
 
 heartpoints_onTestComplete() { local failureOrSuccess=$1
     echo """
 
-    Test Suite ${failureOrSuccess}  -  more details in $(heartpoints_testOutputFile)
+    Test Suite ${failureOrSuccess} - more details in $(heartpoints_testOutputFile)
 
     """
 }
 
-heartpoints_testOutputFile() {
-    echo "test-result.txt"
-}
-
 test_output() { local output=$1
-    echo $output >> "$(heartpoints_testOutputFile)"
-    echo $output
+    echo "$output" >> "$(heartpoints_testOutputFile)"
+    echo "$output"
 }
 
 heartpoints_test() { local baseUrl=$1
     set -e
     trap "heartpoints_onTestComplete failed; false" ERR
-    echo "" > "$(heartpoints_testOutputFile)"
-    echo "Testing..."
-    echo "Test homepage html file is 200..."
-    curl -L --insecure "${baseUrl}" --fail -o /dev/null >> "$(heartpoints_testOutputFile)"
-    echo "passed"
-    echo "" 
-    echo "Test bundle.js file is 200..." 
-    curl -L --insecure "${baseUrl}/bundle.js" --fail -o /dev/null >> "$(heartpoints_testOutputFile)"
-    echo "passed"
-    echo "" 
-    echo "Test commitSha presence in header matches current sha ($(git_commitSha)):" 
+    echo "Testing..." > "$(heartpoints_testOutputFile)"
+    test_output "Test homepage html file is 200..."
+    test_output "$(curl -L --insecure "${baseUrl}" --fail -o /dev/null)"
+    test_output "passed"
+    test_output "" 
+    test_output "Test bundle.js file is 200..." 
+    test_output "$(curl -L --insecure "${baseUrl}/bundle.js" --fail -o /dev/null)"
+    test_output "passed"
+    test_output "" 
+    test_output "Test commitSha presence in header matches current sha ($(git_currentSha)):" 
     local headerOutput="$(curl -L --insecure -I "${baseUrl}")"
-    echo "$headerOutput"
-    echo "$headerOutput" | grep -i "commitSha: $(git_commitSha)"
-    echo "passed"
+    test_output "$headerOutput"
+    test_output "$headerOutput" | grep -i "commitSha: $(git_currentSha)"
+    test_output "passed"
     heartpoints_onTestComplete "passed"
 }
 
-heartpoints_onMasterMerge() { export herokuApiKey
-    local imageURI="registry.heroku.com/$(heroku_applicationName)/web:$(git_commitSha)"
+heartpoints_onMasterMerge() {
+    local imageURI="gcr.io/heartpoints-org/heartpoints.org:$(git_currentSha)"
     heartpoints_buildAndTagImage "${imageURI}"
-    heartpoints_pushImage "${imageURI}" "${herokuApiKey}"
-    heartpoints_deploy $(git_commitSha) "${herokuApiKey}"
+    heartpoints_pushImage "${imageURI}"
+    heartpoints_deploy $(git_currentSha)
     local secondsToWait=45
     echo "waiting ${secondsToWait} seconds for deploy to complete before testing..."
     sleep ${secondsToWait}
-    heartpoints_test "http://www.heartpoints.org"
+    heartpoints_test "http://www.heartpointsfixthis.org" # TODO: fix t his link!!!!!
 }
 
-heartpoints_testAgainstMinikube() {
+heartpoints_minikubeRunTests() {
     heartpoints_test "$(heartpoints_urlOfMinikubeWebsite)"
 }
 
 heartpoints_createGKECluster() {
     withinCloudSDK ./heartpoints.sh createGKECluster_commands
-}
-
-cicdServiceAccountEmail() {
-    echo "cicd-353@heartpoints-org.iam.gserviceaccount.com"
 }
 
 withinCloudSDK() { local commands=$@
@@ -192,11 +139,8 @@ heartpoints_gcloud_kubectl() { local args=$@
 heartpoints_kubectl_commands() { local args=$@
     gke_cicdAccountLogin
     gcloud container clusters get-credentials heartpoints-org --zone us-central1-a --project heartpoints-org
+    kubectl_install
     kubectl $args
-}
-
-gke_cicdAccountLogin() {
-    gcloud auth activate-service-account "$(cicdServiceAccountEmail)" --key-file=heartpoints-org-a5b59b6b4963.json
 }
 
 heartpoints_createGKECluster_commands() {
@@ -227,73 +171,18 @@ heartpoints_runServer() {
     heartpoints_yarn start
 }
 
-heartpoints_production() {
-    heartpoints_prepareForRun
-    export PORT
-}
-
-git_commitSha() {
-    echo "$(git rev-parse HEAD)"
-}
-
-strings_are_equal() { local string1=$1; local string2=$2
-    [ "${string1}" = "${string2}" ]
-}
-
-heartpoints_pushImage() { local imageURI=$1; local herokuApiKey=$2
-    docker login --username=tom@cleveweb.com --password="${herokuApiKey}" registry.heroku.com
+heartpoints_pushImage() { local imageURI=$1
+    gcloud_login #TODO: fix this login to be non-interactive and stuff
     docker push "${imageURI}"
 }
 
 heartpoints_manualDeploy() { local gitSha=$1
-    heroku_login
-    heartpoints_deploy "${gitSha}" "$(heroku_authTokenForAlreadyLoggedInSession)"
+    gcloud_login
+    heartpoints_deployToKubernetes "${gitSha}"
 }
 
-heroku_authTokenForAlreadyLoggedInSession() {
-    heroku auth:token
-}
-
-heartpoints_deploy() { local gitSha=$1; local herokuApiKey=$2
-    local imageURI="registry.heroku.com/$(heroku_applicationName)/web:${gitSha}"
-    local imageId=$(docker inspect ${imageURI} --format={{.Id}})
-    curl -n -X PATCH https://api.heroku.com/apps/$(heroku_applicationName)/formation \
-        -d '{"updates":[{"type":"web","docker_image":"'"${imageId}"'"}]}' \
-        -H "Content-Type: application/json" \
-        -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
-        -H "Authorization: Bearer ${herokuApiKey}"
-}
-
-brew_cask_caskIsInstalled() { local caskName=$1
-    brew cask list | grep "${caskName}" > /dev/null 2>&1
-}
-
-brew_cask_installCaskroom() {
-    if ! brew info cask &>/dev/null; then
-        brew tap caskroom/cask
-    fi
-}
-
-brew_cask_installCask() { local caskName=$1
-    brew_cask_installCaskroom
-    if command_does_not_exist "${caskName}"; then
-        brew cask install "${caskName}"
-    fi
-}
-
-heartpoints_kubectl() { local args=$@
-    kubectl_install
-    kubectl $args
-}
-
-kubectl_install() {
-    if command_does_not_exist kubectl; then
-        brew install kubernetes-cli
-    fi
-}
-
-heartpoints_dockerImageTag() {
-    echo "sha-$(git_commitSha)"
+heartpoints_deployToKubernetes() { local image=$1
+    cat heartpoints-k8s.yml | sed "s/{{image}}/${image}/" | kubectl apply -f -
 }
 
 heartpoints_pointToAndRunMinikubeDockerDaemon() {
@@ -301,43 +190,27 @@ heartpoints_pointToAndRunMinikubeDockerDaemon() {
     eval $(minikube docker-env)
 }
 
-heartpoints_buildAndDeployToMinikube() {
+heartpoints_minikubeBuildDeployTest() {
     heartpoints_pointToAndRunMinikubeDockerDaemon
-    heartpoints_buildAndTagImage "heartpoints.org:latest"
-    heartpoints_deployToMinikube
+    heartpoints_buildAndTagImage "heartpoints.org:$(git_currentSha)"
+    heartpoints_deployToKubernetes "heartpoints.org:$(git_currentSha)"
     local minikubeStartupTimout=30
     echo "deployment complete... waiting ${minikubeStartupTimout} seconds before running test (ctrl+c to safely exit)"
     sleep ${minikubeStartupTimout}
-    heartpoints_testAgainstMinikube
+    heartpoints_minikubeRunTests
 }
 
-heartpoints_destroyMinikubeEnvironment() {
-    heartpoints_minikube_stop
+heartpoints_minikubeDestroyEnvironment() {
+    heartpoints_minikube delete
 }
 
 heartpoints_urlOfMinikubeWebsite() {
     minikube_install > /dev/null 2>&1
-    echo "http://$(minikube ip)/"
+    echo "https://$(minikube ip)"
 }
 
-heartpoints_openMinikubeWebsite() {
+heartpoints_minikubeOpenWebsite() {
     open "$(heartpoints_urlOfMinikubeWebsite)"
-}
-
-heartpoints_deployToMinikube() {
-    heartpoints_kubectl apply -f heartpoints-k8s.yml
-}
-
-virtualbox_install() {
-    brew_cask_installCask virtualbox
-}
-
-minikube_install() {
-    kubectl_install
-    virtualbox_install
-    if command_does_not_exist minikube; then
-        curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.32.0/minikube-darwin-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
-    fi
 }
 
 heartpoints_minikube() { local args=$@
@@ -362,46 +235,101 @@ heartpoints_minikube_isRunning() {
     heartpoints_minikube status | grep "host: Running"
 }
 
-heroku_cli() { local args=$@
-    if command_does_not_exist heroku; then
-        (brew install heroku/brew/heroku)
-    fi
-    heroku $args
-}
-
 heartpoints_model() {
     heartpoints_yarn install
     heartpoints_yarn ts-node src/heartpoints-cli.ts
 }
 
-git_working_directory_is_clean() {
-    [ -z "$(git status --porcelain)" ]
+# Authentication
+
+
+gke_cicdAccountLogin() {
+    gcloud_install
+    gcloud auth activate-service-account "$(cicdServiceAccountEmail)" --key-file=heartpoints-org-a5b59b6b4963.json
+}
+
+gcloud_login() { # this may not be necessary if we use gke_cicdAccountLogin
+    gcloud_install
+    gcloud auth login # TODO: fix this to be non-interactive!!
+    gcloud config set project heartpoints-org
+    gcloud auth configure-docker
+}
+
+# Misc functions
+
+brew_cask_caskIsInstalled() { local caskName=$1
+    brew cask list | grep "${caskName}" > /dev/null 2>&1
+}
+
+brew_cask_installCask() { local caskName=$1
+    brew_cask_installCaskroom
+    if command_does_not_exist "${caskName}"; then
+        brew cask install "${caskName}"
+    fi
+}
+
+brew_cask_installCaskroom() {
+    if ! brew info cask &>/dev/null; then
+        brew tap caskroom/cask
+    fi
 }
 
 command_does_not_exist() { local possibleCommand=$1
     ! [ -x "$(command -v ${possibleCommand})" ]
 }
 
-nvm_script_path() {
-    echo "$(nvm_script_dir)/nvm.sh"
-}
-
-nvm_script_dir() {
-    echo "$HOME/.nvm"
-}
-
-nvm_load_existing_nvm() {
-    export NVM_DIR="$(nvm_script_dir)"
-    [ -s "$(nvm_script_path)" ] && \. "$(nvm_script_path)"
-}
-
 file_does_not_exist() { local possibleFilePath=$1
     ! [ -e "${possibleFilePath}" ]
+}
+
+gcloud_install() {
+    brew cask install google-cloud-sdk 
+}
+
+git_currentSha() {
+    echo "$(git rev-parse HEAD)"
+}
+
+git_working_directory_is_clean() {
+    [ -z "$(git status --porcelain)" ]
+}
+
+kubectl_install() {
+    if command_does_not_exist kubectl; then
+        brew install kubernetes-cli
+    fi
+}
+
+minikube_install() {
+    kubectl_install
+    virtualbox_install
+    if command_does_not_exist minikube; then
+        curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.32.0/minikube-darwin-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+    fi
+}
+
+nodejs_ensureCorrectVersion() {
+    if command_does_not_exist "node" || ! strings_are_equal "$(node -v)" "$(cat .nvmrc)"; then
+        nvm_installAndUseVersionInNvmRC
+    fi
+}
+
+npm_cli() { local args=$@
+    nodejs_ensureCorrectVersion   
+    npm ${args}
 }
 
 nvm_download_and_install() {
     touch ~/.bash_profile
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+}
+
+nvm_installAndUseVersionInNvmRC() {
+    nvm_load
+    set +e
+    nvm install 
+    set -e
+    nvm use
 }
 
 nvm_load() {
@@ -414,5 +342,42 @@ nvm_load() {
         set -e
     fi
 }
+
+nvm_load_existing_nvm() {
+    export NVM_DIR="$(nvm_script_dir)"
+    [ -s "$(nvm_script_path)" ] && \. "$(nvm_script_path)"
+}
+
+nvm_script_dir() {
+    echo "$HOME/.nvm"
+}
+
+nvm_script_path() {
+    echo "$(nvm_script_dir)/nvm.sh"
+}
+
+string_is_empty() { local possiblyEmptyString=$1
+    [[ -z "${possiblyEmptyString}" ]]
+}
+
+strings_are_equal() { local string1=$1; local string2=$2
+    [ "${string1}" = "${string2}" ]
+}
+
+virtualbox_install() {
+    brew_cask_installCask virtualbox
+}
+
+# "Variables"
+
+cicdServiceAccountEmail() {
+    echo "cicd-353@heartpoints-org.iam.gserviceaccount.com"
+}
+
+heartpoints_testOutputFile() {
+    echo "test-result.txt"
+}
+
+# Entrypoint
 
 heartpoints $@
