ae85baccd676f0b661b1fd56957ce949e8ff0e2a
Tommy Sullivan <tommy@classdojo.com>
2019-01-27 20:44:40 +0000

    convert server to typescript with watcher and autorestart

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,13 @@ import * as ReactDOM from "react-dom";
 
 import { Site } from "./components/site";
 
-ReactDOM.render(
-    <Site />,
-    document.getElementById("site")
-);
+const renderApp = ({showSimpleModel, isDev}) => {
+    const navigateToSimpleModel = () => renderApp({showSimpleModel: true, isDev})
+    ReactDOM.render(
+        <Site showSimpleModel={showSimpleModel} navigateToSimpleModel={navigateToSimpleModel} isDev={isDev} />,
+        document.getElementById("site")
+    );
+}
+
+const isDev = window.location.href.indexOf("localhost") > -1;
+renderApp({showSimpleModel: false, isDev });
