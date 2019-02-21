3a70ce32bf85939a7e911e0a785ce89f67535d79
Tommy Sullivan <tommy@classdojo.com>
2019-02-21 04:08:50 +0000

    missed review comments

--- src/components/layouts/Dev.tsx
+++ src/components/layouts/Dev.tsx
@@ -16,5 +16,3 @@ export const Dev = props => {
         <FacebookSessionInfo {...{facebookUserSession}} />
     </React.Fragment>
 }
-
-const If = ({condition, children}) => condition ? children : null;
