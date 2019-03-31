a300d412616b44c8a915c38804537674a09bbcda
Aash <ashankar@heartpoints.org>
2019-03-31 10:24:16 -0700

    Adding Organization to side bar #115

--- src/components/nav/SideNav.tsx
+++ src/components/nav/SideNav.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
-import { Drawer, IconButton, Divider, List, ListItemIcon, ListItemText, withStyles, ListItem, AppBar } from "@material-ui/core";
+import { Drawer, IconButton, Divider, List, ListItemIcon, ListItemText, withStyles, ListItem, AppBar, Collapse } from "@material-ui/core";
 
 import InboxIcon from '@material-ui/icons/MoveToInbox';
+import Language from '@material-ui/icons/Language';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import DonutSmallIcon from '@material-ui/icons/DonutSmall';
+import StarBorder from '@material-ui/icons/StarBorder';
+import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
+import Search from '@material-ui/icons/Search';
+import Favorite from '@material-ui/icons/Favorite';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 import classNames from 'classnames';
 
-export const SideNavUnstyled = ({history, isSideNavOpen, classes, theme, onSideNavCollapseRequested}) => {
+export const SideNavUnstyled = ({history, isSideNavOpen, isSideNavExpanded, onSideNavExpandRequested, classes, theme, onSideNavCollapseRequested}) => {
     return <Drawer
           className={classes.drawer}
           variant="persistent"
@@ -27,6 +34,31 @@ export const SideNavUnstyled = ({history, isSideNavOpen, classes, theme, onSideN
             <ListItem button onClick={() => history.push("/") }>
               <ListItemIcon><InboxIcon /></ListItemIcon>
               <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem button onClick={onSideNavExpandRequested}>
+              <ListItemIcon><Language /></ListItemIcon>
+              <ListItemText primary="Organization" />
+              {isSideNavExpanded ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={isSideNavExpanded} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+             <ListItem button className={classes.nested}>
+              <ListItemIcon>
+                <AddCircleOutline />
+              </ListItemIcon>
+              <ListItemText inset primary="Create New" />
+             </ListItem>
+             <ListItem button className={classes.nested}>
+              <ListItemIcon>
+                <Search />
+              </ListItemIcon>
+              <ListItemText inset primary="Search" />
+             </ListItem>
+            </List>
+            </Collapse>
+            <ListItem button onClick={() => history.push("/") }>
+              <ListItemIcon><Favorite /></ListItemIcon>
+              <ListItemText primary="My Heart Collection" />
             </ListItem>
             <ListItem button onClick={() => history.push("/castleRisk") }>
               <ListItemIcon><DonutSmallIcon /></ListItemIcon>
@@ -53,6 +85,9 @@ const styles = theme => ({
     padding: '0 8px',
     ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
+  },
+  nested: {
+    paddingLeft: theme.spacing.unit * 4,
   },
 });
 
