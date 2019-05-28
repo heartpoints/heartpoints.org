4f1c01feafa4e98341c943bb82910e2c76e9433c
Thomas Sullivan <thomassullivan@Esa-Mattis-MacBook-Air.local>
2019-05-27 18:15:08 -0700

    add alias for hp yarn

--- heartpoints.sh
+++ heartpoints.sh
@@ -257,7 +257,11 @@ hp_prepareForRun() {
     fi
 }
 
-hp_yarn_global_help() { echo "globally install (if needed) - and then call - yarn dependency manager for nodejs"; }
+hp_yarn() { local args="$@"
+    hp_yarn_global "$@"
+}
+
+hp_yarn_help() { echo "globally install (if needed) - and then call - yarn dependency manager for nodejs"; }
 hp_yarn_global() { local args=$@
     if command_does_not_exist "yarn"; then
         npm_cli install yarn -g
