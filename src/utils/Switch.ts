06b5adcefadbf465ad0b24e48aeadc17002f1c09
Thomas Sullivan <thomassullivan@Esa-Mattis-MacBook-Air.local>
2019-05-31 22:04:42 -0700

    uncomment interface

--- src/utils/Switch.ts
+++ src/utils/Switch.ts
@@ -21,9 +21,9 @@ interface ICaseMatch<T, V> {
 }
 
 interface ICaseMatchWithoutInput<V> {
-    // case<R>(condition:boolean, resultToUseIfMatch:R):ISwitchWithoutInput<V | R>
-    // caseLazy<R>(condition:boolean, resultProviderToUseIfMatch:Provider<R>):ISwitchWithoutInput<V | R>
-    // matches<R>(predicate:Provider<boolean>, resultToUseIfMatch:R): ISwitchWithoutInput<V | R>
+    case<R>(condition:boolean, resultToUseIfMatch:R):ISwitchWithoutInput<V | R>
+    caseLazy<R>(condition:boolean, resultProviderToUseIfMatch:Provider<R>):ISwitchWithoutInput<V | R>
+    matches<R>(predicate:Provider<boolean>, resultToUseIfMatch:R): ISwitchWithoutInput<V | R>
     matchesLazy<R>(predicate:Provider<boolean>, resultProviderToUseIfMatch:Provider<R>): ISwitchWithoutInput<V | R>
 }
 
