8e2a699b5673122cfb6a822127baa0ea72b624e3
Thomas Sullivan <thomassullivan@Esa-Mattis-MacBook-Air.local>
2019-06-07 17:22:20 -0700

    made a simple program for johnny

---
+++ src/johnny.js
@@ -0,0 +1,7 @@
+const squareMe = x => x * x
+
+//to run me, just run `node src/johnny.js` from the node command prompt
+
+console.log("Welcome to Johnny's program");
+console.log(squareMe(5))
+console.log(squareMe(6))
