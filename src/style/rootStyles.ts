7c917dbb57e37e141865d5cc0f0c205634e86cb4
Thomas Sullivan <thomassullivan@Thomass-MacBook-Pro.local>
2019-06-25 19:53:47 -0700

    fix spacing and warnings for material ui 4 upgrade

--- src/style/rootStyles.ts
+++ src/style/rootStyles.ts
@@ -3,7 +3,7 @@ import { drawerWidth } from "./drawerWidth";
 export const rootStyles = theme => ({
   content: {
     flexGrow: 1,
-    padding: theme.spacing.unit * 3,
+    padding: theme.spacing(3),
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
