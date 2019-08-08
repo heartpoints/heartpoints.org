ecb9a8469174445043bac27d6ac972459f7c7303
Thomas Sullivan <thomassullivan@Thomass-MacBook-Pro.local>
2019-08-07 22:15:53 -0400

    remove unused navTo

--- src/frontEnd/buttons/EditButton.tsx
+++ src/frontEnd/buttons/EditButton.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import EditIcon from '@material-ui/icons/Edit';
 import { IconButton, Tooltip } from '@material-ui/core';
 
-export const EditButton = ({ navTo, onClick }) => <Tooltip title="Edit Organization" placement="right">
+export const EditButton = ({ onClick }) => <Tooltip title="Edit Organization" placement="right">
     <IconButton>
         <EditIcon onClick={onClick} />
     </IconButton>
--- src/frontEnd/forms/viewEditToggleables/Text.tsx
+++ src/frontEnd/forms/viewEditToggleables/Text.tsx
@@ -3,11 +3,11 @@ import { InputForField } from "../InputForField"
 import { EditButton } from "../../buttons/EditButton";
 
 export const Text = (props) => {
-    const {isEditable, isEditMode, field, navTo } = props
+    const { isEditMode, field } = props
     return isEditMode
         ? <React.Fragment>
             <InputForField {...{field}} {...props} />
-            <EditButton {...{navTo, onClick: alert}} />
+            <EditButton {...{onClick: alert}} />
         </React.Fragment>
         : <React.Fragment>{field.value}</React.Fragment>
 }
