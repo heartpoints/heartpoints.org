a75ff46e6ec5636108add3b3ea24d5f3e3a589e2
Tommy Sullivan <tom@tommysullivan.me>
2019-09-28 00:10:25 -0700

    callWith spacing

--- src/utils/composition/callWith.ts
+++ src/utils/composition/callWith.ts
@@ -1,4 +1,8 @@
 import { Consumer } from "../axioms/Consumer";
 
 export type CallWith = <T>(eventualParameter:T) => (functionToCall:Consumer<T>) => void;
-export const callWith:CallWith = <T>(eventualParameter:T) => (functionToCall:Consumer<T>) => functionToCall(eventualParameter);
+
+export const callWith:CallWith = 
+    <T>(eventualParameter:T) => 
+    (functionToCall:Consumer<T>) => 
+    functionToCall(eventualParameter);
