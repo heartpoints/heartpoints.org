97f4ff91a4332d8d345a0958a1ff8e016328286a
Tommy Sullivan <tom@tommysullivan.me>
2019-09-28 00:14:44 -0700

    simplify stringToMaybeBoolean

--- src/utils/strings/stringToMaybeBoolean.ts
+++ src/utils/strings/stringToMaybeBoolean.ts
@@ -2,7 +2,7 @@ import { Switch } from "../switch/Switch";
 import { IMaybe } from "../maybe/IMaybe";
 
 export type StringToMaybeBoolean = (s:string) => IMaybe<boolean>
-export const stringToMaybeBoolean: StringToMaybeBoolean = (s: string) => Switch.when(s)
-    .matches(s => s.toLowerCase().trim() === "true", true)
-    .matches(s => s.toLowerCase().trim() === "false", false)
+export const stringToMaybeBoolean: StringToMaybeBoolean = (s: string) => Switch.when(s.toLowerCase().trim())
+    .matches(s => s === "true", true)
+    .matches(s => s === "false", false)
     .result;
