8f53147733551e5c37051cb6d7f34b66521aa23b
Tommy Sullivan <tom@tommysullivan.me>
2019-09-28 23:49:09 -0700

    fix bug in allMaybes so that more graceful lack of LB forwarding will occur

--- src/webServer/middleware/immutableMiddlewares.ts
+++ src/webServer/middleware/immutableMiddlewares.ts
@@ -1,7 +1,5 @@
 import { commitShaHeaderMiddleware } from "./commitShaHeaderMiddleware";
-// import { loadBalancerHttpRedirector } from "./loadBalancerHttpRedirector";
+import { loadBalancerHttpRedirector } from "./loadBalancerHttpRedirector";
 import { requestLogMiddleware } from "./requestLogMiddleware";
 
-//todo: fix loadBalancerHttpRedirector and re-add
-// export const immutableMiddlewares = [requestLogMiddleware, commitShaHeaderMiddleware, loadBalancerHttpRedirector];
-export const immutableMiddlewares = [requestLogMiddleware, commitShaHeaderMiddleware];
+export const immutableMiddlewares = [requestLogMiddleware, commitShaHeaderMiddleware, loadBalancerHttpRedirector];
