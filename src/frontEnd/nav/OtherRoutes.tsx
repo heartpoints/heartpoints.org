0e096d98f5705deaf2b054e2e95e330506085400
nishijain <nishijain2512@gmail.com>
2019-10-10 10:05:51 -0400

    Updtd /dev route to load homepage on login/logout

--- src/frontEnd/nav/OtherRoutes.tsx
+++ src/frontEnd/nav/OtherRoutes.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
-
 import { HomePage } from "../homePage/HomePage";
 import { SearchBar as VolunteeringSearchBar } from "../volunteering/SearchBar";
-import { FacebookLoginLogout } from "../facebook/FacebookLoginLogout";
 import { CastleRisk } from "../castleRisk/CastleRisk";
 import { Router } from "./Router";
 import { RestGuru } from "../RestGuru/RestGuru";
 
 export const OtherRoutes = (url, props, router: Router) => router
   .case("/", <HomePage />)
-  .case("/dev", <FacebookLoginLogout {...props} />)
+  .case("/dev", <HomePage />)
   .case("/castleRisk", <CastleRisk {...props.castleRisk} {...props} />)
   .case("/rest-guru", <RestGuru {...props} />)
   .case("/volunteering/search", <VolunteeringSearchBar {...props} />);
