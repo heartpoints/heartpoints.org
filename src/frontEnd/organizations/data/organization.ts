8bd97ec2d9058b860bc36ff81b255fe6b3dbe640
nishijain <nishijain2512@gmail.com>
2019-10-29 12:05:35 -0400

    upded data file & UI for volunteering accordingly

--- src/frontEnd/organizations/data/organization.ts
+++ src/frontEnd/organizations/data/organization.ts
@@ -1,8 +1,15 @@
+export interface VolOpportunity {
+    jobID: string,
+    jobTitle: string,
+    jobDescription: string
+}
+
 export interface Organization {
     creatorEmail: string,
     href: string,
     imageThumbnailURL: string,
     title: string,
     mission: string,
-    homepage: string
+    homepage: string,
+    volOpportunities: Array<VolOpportunity>
 }
