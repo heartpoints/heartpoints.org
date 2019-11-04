62762d62e0ccd9e285ec9476609feabbd6f2daae
Tommy Sullivan <tommy@heartpoints.org>
2019-11-04 10:54:15 -0800

    added flatten, flatMap, toString, join, ListOfLists to functional lis… (#233)
    * added flatten, flatMap, toString, join, ListOfLists to functional list type
    
    * fix failing tests

--- src/utils/list/ListOfLiterals.ts
+++ src/utils/list/ListOfLiterals.ts
@@ -1,4 +1,9 @@
 import { EmptyList } from "./EmptyList";
 import { IList } from "./IList";
 
-export const ListOfLiterals = <T>(...items: Array<T>): IList<T> => items.reduce((listSoFar, item) => listSoFar.push(item), EmptyList as IList<T>);
+export const ListOfLiterals = 
+    <T>(...items: Array<T>): IList<T> => 
+    items.reduce(
+        (listSoFar, item) => listSoFar.push(item), 
+        EmptyList as IList<T>
+    );
--- src/utils/list/NonEmptyList.ts
+++ src/utils/list/NonEmptyList.ts
@@ -11,9 +11,14 @@ export const NonEmptyList = <T, S>(head: T, tail: IList<S>): IList<T | S> => ({
     isEmpty: false,
     notEmpty: true,
     get asArray() {
-        return this.reduce((arraySoFar, item) => [...arraySoFar, item], [] as Array<T | S>);
+        return this.reduce(
+            (arraySoFar, item) => [...arraySoFar, item],
+            [] as Array<T | S>
+        )
     },
-    toString() { return this.asArray.toString(); },
+    toString() { return `[${this.map((e:any) => e.toString()).join(", ")}]` },
     any: predicate => predicate(head) ? true : tail.any(predicate),
     [Symbol.iterator]() { return this.asArray[Symbol.iterator](); },
+    append: <S>(otherList:IList<S>) => NonEmptyList(head, tail.append(otherList)),
+    join(delimiter) { return this.asArray.join(delimiter) }
 });
