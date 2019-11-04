ae00c724ebf9dbff96eff75c55f32796b8dc9d8e
Tommy Sullivan <tom@tommysullivan.me>
2019-11-04 12:41:55 -0800

    simplify proptype for ViewVolunteeringOption

--- src/frontEnd/volunteering/ViewVolunteeringOption.tsx
+++ src/frontEnd/volunteering/ViewVolunteeringOption.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import  { LoadVolunteeringOption } from "./LoadVolunteeringOption";
-import { Organizations } from '../organizations/data/Organizations';
 import { WithNavTo } from '../nav/WithNavTo';
 import { WithUrl } from '../../utils/url/WithUrl';
 import { NotFound } from '../nav/NotFound';
 import { findOrgOpportunityPairByJobId } from './findOrgOpportunityPairByJobId';
 import { combineOrgOppPairToSinglePropsObject } from './combineOrgOppPairToSinglePropsObject';
+import { WithOrganizations } from '../organizations/data/WithOrganizations';
 
-type ViewVolunteeringOptionProps = { organizations: Organizations } & WithNavTo & WithUrl
+type ViewVolunteeringOptionProps = WithNavTo & WithUrl & WithOrganizations
 export const ViewVolunteeringOption = ({url, organizations, navTo}:ViewVolunteeringOptionProps) => {
     const selectedJobID = (url.path).substring(14);
     const possiblyMatchingOrgVolPair = findOrgOpportunityPairByJobId(organizations, selectedJobID)
