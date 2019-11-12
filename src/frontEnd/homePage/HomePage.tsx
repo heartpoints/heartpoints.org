e5a1e808ca2e5a72189fecaaec051681276607e0
Patrick Metsch <patrickmetsch@patricks-mbp.attlocal.net>
2019-11-12 16:25:15 -0500

    help us build... resizes with on mobile

--- src/frontEnd/homePage/HomePage.tsx
+++ src/frontEnd/homePage/HomePage.tsx
@@ -9,11 +9,15 @@ const containerStyle = {
     alignItems: "center"
 }
 
+const subtitleSize = window.innerWidth <= 480
+    ? "subtitle1"
+    : "h4"
+
 export const HomePage = ({shouldShowSnackbar, snackbarText}) => { 
     return <Container style={containerStyle}>
         <Card style={{textAlign: "center", verticalAlign: "middle"}}>
             <img width="100%" src="/images/logo.png" />
-            <Typography variant="h4">help us build something amazing!</Typography>
+            <Typography variant={subtitleSize}>help us build something amazing!</Typography>
             <Space />
             <Typography variant="h6">
                 <a href="mailto:info@heartpoints.org">info@heartpoints.org</a> 
